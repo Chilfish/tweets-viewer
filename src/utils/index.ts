6653c5476cb2a4f20c738c41c5e21ed5ca42aac9
Chilfish <chill4fish@gmail.com>
2024-09-01 00:07:03 +0800

    fix: network checking

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,10 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 export async function canGoogle() {
   try {
     const aborter = new AbortController()
-    setTimeout(() => aborter.abort('Timeout 2s'), 2000)
+    setTimeout(() => aborter.abort('Timeout 1.5s'), 1500)
 
     const urls = [
+      // 居然有时能直连推特图床
       'https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg',
+      'https://x.com',
     ]
     console.log('Checking network... ping to', urls)
 
