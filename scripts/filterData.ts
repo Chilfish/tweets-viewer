5e0b92d7a1a0e1c0cf0c396d8ff7f6b8b1da905e
Chilfish <chill4fish@gmail.com>
2024-10-02 14:25:36 +0800

    fix: the scripts workflow

---
+++ scripts/filterData.ts
@@ -0,0 +1,110 @@
+import glob from 'fast-glob'
+
+import { dir, readJson, uniqueObj, writeJson } from './utils'
+
+interface User {
+  name: string // as id
+  screen_name: string
+  avatar_url: string
+}
+
+const removed = [
+  'bookmark_count',
+  'bookmarked',
+  'favorited',
+  'retweeted',
+  'url',
+]
+
+const includeRT = true
+const name = process.argv[2]
+if (!name) {
+  console.error('Please provide a twitter name.')
+  process.exit(1)
+}
+const folder = dir(`D:/Downloads/tweet-data/${name}`)
+
+/**
+ * data.json exported from https://github.com/prinsss/twitter-web-exporter
+ */
+const jsons = await glob(`${folder}/*.json`)
+  .then(files => files.filter(name => !name.includes('data-')))
+
+let data: any[] = []
+
+if (!jsons.length) {
+  console.error('No data found.')
+  process.exit(1)
+}
+
+console.log(`Merging ${jsons.length} files...`, jsons)
+
+for (const json of jsons) {
+  const _data = await readJson(json)
+  data.push(..._data)
+}
+
+const mergedData = uniqueObj(data, 'id').sort((a, b) => b.id.localeCompare(a.id))
+
+console.log(mergedData.length, data.length)
+
+await writeJson(`${folder}/data-merged.json`, mergedData)
+
+data = data.map((el) => {
+  if (el.full_text.startsWith('RT @') && !includeRT) {
+    return null
+  }
+
+  removed.forEach((key) => {
+    delete el[key]
+  })
+  el.media = el.media.map((media: any) => {
+    let url = media.original
+    if (media.type === 'video')
+      url = media.original
+
+    return url
+  })
+
+  el.full_text = el.full_text
+    .replace(/\xA0|\u3000/g, ' ') // 不可见空格
+
+  el.views_count = +el.views_count || 0
+
+  if (el.in_reply_to) {
+    const tweet = data.find(tweet => tweet.id === el.in_reply_to)
+    if (tweet) {
+      el.in_reply_to = {
+        id: tweet.id,
+        name: tweet.name,
+      }
+    }
+  }
+
+  el.created_at = el.created_at
+    .replace(/_/, ' ')
+    .replace(/ [+-]\d+/, '')
+
+  return el
+})
+  .filter(Boolean)
+  .sort((a, b) => a.created_at.localeCompare(b.created_at))
+
+const user = {
+  name: data[0].screen_name,
+  screen_name: data[0].name,
+  avatar_url: data[0].profile_image_url,
+} as User
+
+data.forEach((el) => {
+  delete el.screen_name
+  delete el.name
+  delete el.profile_image_url
+})
+
+await writeJson(`D:/Codes/static/tweet/data-${user.name}.json`, {
+  user,
+  tweets: data,
+}, 'write', 0)
+
+console.log(`Done! ${data.length} tweets saved.`)
