24786aa96d9e0baec15fd62d3791a742300e5fcc
Chilfish <chill4fish@gmail.com>
2024-12-06 11:42:36 +0800

    update deps

--- src/components/ui/avatar/index.ts
+++ src/components/ui/avatar/index.ts
@@ -1,4 +1,4 @@
-import { cva, type VariantProps } from 'class-variance-authority'
+import { cva, type VariantProps } from 'cva'
 
 export { default as Avatar } from './Avatar.vue'
 export { default as AvatarFallback } from './AvatarFallback.vue'
